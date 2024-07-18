de6530405aa35cf56407ae1dbf7d454fb1ca5567
Syed-Anas-Ahmed <muhananas@gmail.com>
2024-07-18 13:24:41 +0500

    Update API URL in env.js file

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 //export const url = "https://hawk.greenstar.org.pk:8441/tabib/";
-export const url = "https://tabibportal.azurewebsites.net/tabib/";
+export const url = "https://tabib.azurewebsites.net/";
 //export const url = "http://192.168.18.7:8083/altabibconnect/";
