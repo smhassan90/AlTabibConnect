4e022d65c2ee628db620dd43a8c08da0ab09e5aa
Syed-Anas-Ahmed <muhananas@gmail.com>
2024-07-25 19:50:05 +0500

    Add confirmation message on Removing family member

--- app/(auth)/(tabs)/(family)/index.tsx
+++ app/(auth)/(tabs)/(family)/index.tsx
@@ -8,7 +8,6 @@ import {
   colors,
   fontFamily,
   fontSizes,
-  spacingM,
   spacingPrim,
   spacingS,
 } from "~/app/styles";
@@ -18,8 +17,11 @@ import { router } from "expo-router";
 import * as SecureStore from "expo-secure-store";
 import Header from "~/app/components/ParentView";
 import { HeartLoader } from "~/app/components/Animations";
-import { color } from "@tamagui/themes";
-import { ALERT_TYPE, Dialog } from "react-native-alert-notification";
+import {
+  ALERT_TYPE,
+  AlertNotificationRoot,
+  Dialog,
+} from "react-native-alert-notification";
 
 const Page = () => {
   const [loading, setLoading] = useState(true);
@@ -38,7 +40,7 @@ const Page = () => {
         .get(`${url}getFamily?token=${token}`)
         .then((res) => {
           const patients = res.data.data.patients;
-          console.log("Family members: ", JSON.stringify(patients, null, 2));
+          //console.log("Family members: ", JSON.stringify(patients, null, 2));
 
           const updatedPatients = patients.map((patient: any) => {
             const dobDate = parseDateString(patient.dob);
@@ -58,10 +60,10 @@ const Page = () => {
           });
           setPatients(updatedPatients);
           setLoading(false);
-          console.log(
-            "Updated Family members: ",
-            JSON.stringify(updatedPatients, null, 2),
-          );
+          // console.log(
+          //   "Updated Family members: ",
+          //   JSON.stringify(updatedPatients, null, 2),
+          // );
         })
         .catch((err) => {
           console.log("Error getting family: ", err);
@@ -76,12 +78,28 @@ const Page = () => {
     router.push("/(auth)/(tabs)/(family)/getHistory");
   };
 
+  const confirmRemoveFamily = (patientId: string) => {
+    Dialog.show({
+      type: ALERT_TYPE.WARNING,
+      title: "Confirm Removal",
+      textBody: "Are you sure you want to remove this family member?",
+      button: "Yes",
+      onPressButton: () => {
+        removeFamily(patientId);
+        Dialog.hide();
+      },
+    });
+  };
+
   const removeFamily = (patientId: string) => {
     axios
       .get(`${url}removeFamilyMember?token=${token}&patientId=${patientId}`)
       .then((res) => {
-        //console.log("Remove Family Response: ", JSON.stringify(res.data, null, 2));
-        if (res.data.status === 200) {
+        console.log(
+          "Remove Family Response: ",
+          JSON.stringify(res.data, null, 2),
+        );
+        if (res.data.status === "200") {
           Dialog.show({
             type: ALERT_TYPE.SUCCESS,
             title: "Family Member Removed",
@@ -94,7 +112,7 @@ const Page = () => {
         } else {
           Dialog.show({
             type: ALERT_TYPE.DANGER,
-            title: "Error removing family member",
+            title: "Error removing family member 1",
             textBody: "Something went wrong",
           });
           setTimeout(() => {
@@ -105,153 +123,160 @@ const Page = () => {
       .catch((error) => {
         Dialog.show({
           type: ALERT_TYPE.DANGER,
-          title: "Error removing family member",
+          title: "Error removing family member 2",
           textBody: "Something went wrong",
         });
-        console.error("Error removing family member: ", error);
+        console.error("Error removing family member 3: ", error);
         setTimeout(() => {
           Dialog.hide();
         }, 2000);
       });
   };
+
   return (
-    <View style={{ flex: 1, backgroundColor: colors.primary }}>
-      <Header>
-        <MenuBar title="My Family" />
-      </Header>
-      <YStack paddingTop={10} flex={1} paddingHorizontal={10}>
-        <TouchableOpacity
-          activeOpacity={0.8}
-          onPress={() => router.push("/(auth)/(tabs)/(family)/AddFamily")}
-          style={[buttons.secBtn]}
-        >
-          <Text color={"white"} fontFamily={fontFamily.bold}>
-            Add Family
-          </Text>
-        </TouchableOpacity>
-        {loading ? (
-          <View
-            style={{
-              alignSelf: "center",
-              gap: spacingPrim,
-              position: "absolute",
-              height: "100%",
-              width: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+    <AlertNotificationRoot>
+      <View style={{ flex: 1, backgroundColor: colors.primary }}>
+        <Header>
+          <MenuBar title="My Family" />
+        </Header>
+        <YStack paddingTop={10} flex={1} paddingHorizontal={10}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => router.push("/(auth)/(tabs)/(family)/AddFamily")}
+            style={[buttons.secBtn]}
           >
-            <Text
-              color={colors.white}
-              fontFamily={"ArialB"}
-              fontSize={fontSizes.L}
-            >
-              Getting Family
+            <Text color={"white"} fontFamily={fontFamily.bold}>
+              Add Family
             </Text>
-            <HeartLoader />
-          </View>
-        ) : (
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            refreshControl={
-              <RefreshControl
-                refreshing={refresh}
-                onRefresh={() => setRefresh(true)}
-                colors={[colors.yellow]}
-                tintColor={colors.white}
-              />
-            }
-            contentContainerStyle={{ gap: 10, paddingVertical: 10 }}
-            horizontal={false}
-            decelerationRate={"normal"}
-            data={patients}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <YStack
-                backgroundColor={colors.white}
-                borderColor={colors.white}
-                borderRadius={spacingPrim}
-                borderWidth={2}
-                padding={spacingPrim}
-                gap={spacingPrim}
+          </TouchableOpacity>
+          {loading ? (
+            <View
+              style={{
+                alignSelf: "center",
+                gap: spacingPrim,
+                position: "absolute",
+                height: "100%",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                color={colors.white}
+                fontFamily={"ArialB"}
+                fontSize={fontSizes.L}
               >
-                <XStack>
-                  <Image
-                    source={
-                      item.age > 18 && item.gender === "male"
-                        ? require("~/assets/man.png")
-                        : item.age > 18 && item.gender === "female"
-                          ? require("~/assets/woman.png")
-                          : item.age <= 18 && item.gender === "male"
-                            ? require("~/assets/boy.png")
-                            : require("~/assets/girl.png")
-                    }
-                    style={{ borderRadius: 50, width: 100, height: 100 }}
-                  />
-                  <YStack justifyContent="center" paddingLeft gap={spacingPrim}>
-                    <XStack marginLeft={5} gap={5}>
-                      <Text
-                        fontFamily={"ArialB"}
-                        fontSize={fontSizes.SM}
-                        color={colors.yellow}
-                      >
-                        Name:
-                      </Text>
-                      <Text
-                        fontFamily={"ArialB"}
-                        fontSize={fontSizes.SM}
-                        color={colors.primary}
-                        maxWidth={200}
-                      >
-                        {item.name}
-                      </Text>
-                    </XStack>
+                Getting Family
+              </Text>
+              <HeartLoader />
+            </View>
+          ) : (
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              refreshControl={
+                <RefreshControl
+                  refreshing={refresh}
+                  onRefresh={() => setRefresh(true)}
+                  colors={[colors.yellow]}
+                  tintColor={colors.white}
+                />
+              }
+              contentContainerStyle={{ gap: 10, paddingVertical: 10 }}
+              horizontal={false}
+              decelerationRate={"normal"}
+              data={patients}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <YStack
+                  backgroundColor={colors.white}
+                  borderColor={colors.white}
+                  borderRadius={spacingPrim}
+                  borderWidth={2}
+                  padding={spacingPrim}
+                  gap={spacingPrim}
+                >
+                  <XStack>
+                    <Image
+                      source={
+                        item.age > 18 && item.gender === "male"
+                          ? require("~/assets/man.png")
+                          : item.age > 18 && item.gender === "female"
+                            ? require("~/assets/woman.png")
+                            : item.age <= 18 && item.gender === "male"
+                              ? require("~/assets/boy.png")
+                              : require("~/assets/girl.png")
+                      }
+                      style={{ borderRadius: 50, width: 100, height: 100 }}
+                    />
+                    <YStack
+                      justifyContent="center"
+                      paddingLeft
+                      gap={spacingPrim}
+                    >
+                      <XStack marginLeft={5} gap={5}>
+                        <Text
+                          fontFamily={"ArialB"}
+                          fontSize={fontSizes.SM}
+                          color={colors.yellow}
+                        >
+                          Name:
+                        </Text>
+                        <Text
+                          fontFamily={"ArialB"}
+                          fontSize={fontSizes.SM}
+                          color={colors.primary}
+                          maxWidth={200}
+                        >
+                          {item.name}
+                        </Text>
+                      </XStack>
 
-                    <XStack marginLeft={5} gap={spacingS}>
-                      <Text
-                        fontFamily={"ArialB"}
-                        fontSize={fontSizes.SM}
-                        color={colors.yellow}
-                      >
-                        Age:
-                      </Text>
-                      <Text
-                        fontFamily={"ArialB"}
-                        fontSize={fontSizes.SM}
-                        color={colors.primary}
-                      >
-                        {item.age}
-                      </Text>
-                    </XStack>
-                  </YStack>
-                </XStack>
+                      <XStack marginLeft={5} gap={spacingS}>
+                        <Text
+                          fontFamily={"ArialB"}
+                          fontSize={fontSizes.SM}
+                          color={colors.yellow}
+                        >
+                          Age:
+                        </Text>
+                        <Text
+                          fontFamily={"ArialB"}
+                          fontSize={fontSizes.SM}
+                          color={colors.primary}
+                        >
+                          {item.age}
+                        </Text>
+                      </XStack>
+                    </YStack>
+                  </XStack>
 
-                <XStack gap={spacingS}>
-                  <Button
-                    onPress={() => checkHistory(item.id.toString())}
-                    backgroundColor={colors.primary}
-                    flex={1}
-                  >
-                    <Text color={colors.white} fontFamily={"ArialB"}>
-                      Check History
-                    </Text>
-                  </Button>
-                  <Button
-                    onPress={() => removeFamily(item.id)}
-                    backgroundColor={colors.yellow}
-                    flex={1}
-                  >
-                    <Text color={colors.white} fontFamily={"ArialB"}>
-                      Remove Member
-                    </Text>
-                  </Button>
-                </XStack>
-              </YStack>
-            )}
-          />
-        )}
-      </YStack>
-    </View>
+                  <XStack gap={spacingS}>
+                    <Button
+                      onPress={() => checkHistory(item.id.toString())}
+                      backgroundColor={colors.primary}
+                      flex={1}
+                    >
+                      <Text color={colors.white} fontFamily={"ArialB"}>
+                        Check History
+                      </Text>
+                    </Button>
+                    <Button
+                      onPress={() => confirmRemoveFamily(item.id)}
+                      backgroundColor={colors.yellow}
+                      flex={1}
+                    >
+                      <Text color={colors.white} fontFamily={"ArialB"}>
+                        Remove Member
+                      </Text>
+                    </Button>
+                  </XStack>
+                </YStack>
+              )}
+            />
+          )}
+        </YStack>
+      </View>
+    </AlertNotificationRoot>
   );
 };
 
