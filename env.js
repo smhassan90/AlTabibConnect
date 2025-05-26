ef4086bac52f9a1bc7c2a802327bed9c09d044f1
OsamaAbdulHafeez <osamahafeez0987@gmail.com>
2025-05-26 18:52:36 +0500

    fix: update API URL in env.js and comment out the previous URL

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 //export const url = "https://hawk.greenstar.org.pk:8441/tabib/";
 //export const url = "https://tabib.azurewebsites.net/";
 // export const url = "http://172.20.144.1:8080/tabib/";
-export const url = `http://altabibserver.fynals.com:8080/tabib/`
+// export const url = `http://altabibserver.fynals.com:8080/tabib/`
+export const url = `http://66.135.60.203:8080/tabib/`
